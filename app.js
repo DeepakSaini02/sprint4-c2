6c05f6e41834b2610594b9aa45493f34b6845b41
Deepak Saini <91534605+DeepakSaini02@users.noreply.github.com>
2021-11-29 14:45:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,56 @@ const connect=()=>{
 
 }
 
+//companySachema-----------
+const companySchema=new mongoose.Schema({
+    company_name:{type:String,required:true},
+    open_vacancy:{type:Number,required:true}
+
+},{
+    timestamps:true,
+    versionKey:false
+})
+
+const Company=mongoose.model("company",companySchema)
+
+// jobSchema------------
+
+const jobSchema=new mongoose.Schema({
+    job_name:{type:String,required:true},
+    job_type:{type:String,required:true},
+    notice_period:{type:Number,required:true},
+    job_location:{type:String,required:true},
+    rating:{type:Number,required:true},
+    company_id:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"company",
+        required:true
+    }
+})
+
+const Job=mongoose.model('job',jobSchema)
+
+
+//basic curd api for company
+
+app.get("/companies",async(req,res)=>{
+    try{
+        const company=await Company.find().lean().exec()
+        res.status(200).send({company})
+    }catch(e){
+        res.status(500).send({message:e.message})
+    }
+})
+
+app.post("/companies",async(req,res)=>{
+    try{
+        const company=await Company.create(req.body)
+        res.status(201).send(company)
+    }catch(e){
+        res.status(500).send({message:e.message})
+    }
+})
+
 
 app.listen(5000,async()=>{
     await connect();
