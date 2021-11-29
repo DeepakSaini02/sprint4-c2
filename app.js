ff5e49f1516d76ca99ae59da60f5a56767ae803f
Deepak Saini <91534605+DeepakSaini02@users.noreply.github.com>
2021-11-29 15:25:24 +0530

    basic crud api

--- app.js
+++ app.js
@@ -3,22 +3,25 @@ const mongoose=require('mongoose')
 const app=express()
 
 app.use(express.json())
+
 const connect=()=>{
     return mongoose.connect("mongodb://localhost:27017/naukri")
 
 }
 
-// companySachema-----------
-const companySchema=new mongoose.Schema({
-    name:{type:String,required:true},
-    vacancy:{type:Number,required:true}
-
+// comapany Sachema-----------
+const compSchema=new mongoose.Schema({
+    comp_name:{type:String,required:true},
+    vacancy:{type:Number,required:true},
+    
 },{
     versionKey:false,
     timestamps:true,
 })
 
-const Company=mongoose.model("company",companySchema)
+const Company=mongoose.model('company',compSchema)
+
+
 
 // jobSchema------------
 
@@ -43,7 +46,17 @@ const Job=mongoose.model('job',jobSchema)
 
 //basic curd api for company
 
-app.get("/jobs",async(req,res)=>{
+app.post("/comp",async(req,res)=>{
+    try{
+        const company=await Company.create(req.body)
+        res.status(201).send(company)
+    }catch(e){
+        res.status(500).send({message:e.message})
+    }
+})
+
+
+app.get("/comp",async(req,res)=>{
     try{
         const company=await Company.find().lean().exec()
         res.status(200).send({company})
@@ -52,15 +65,81 @@ app.get("/jobs",async(req,res)=>{
     }
 })
 
-app.post("/jobs",async(req,res)=>{
+app.get("/comp/:id",async(req,res)=>{
     try{
-        const company=await Job.create(req.body)
-        res.status(201).send(company)
+        const company=await Company.findById(req.params.id).lean().exec()
+        res.status(200).send({company})
     }catch(e){
         res.status(500).send({message:e.message})
     }
 })
 
+
+app.patch("/comp/:id",async(req,res)=>{
+    try{
+        const company=await Company.findByIdAndUpdate(req.params.id,req.body,{new:true}).lean().exec()
+        res.status(200).send({company})
+    }catch(e){
+        res.status(500).send({message:e.message})
+    }
+})
+
+app.delete("/comp/:id",async(req,res)=>{
+    try{
+        const company=await Company.findByIdAndDelete(req.params.id).lean().exec()
+        res.status(200).send({company})
+    }catch(e){
+        res.status(500).send({message:e.message})
+    }
+})
+
+//basic curd api for jobs
+
+app.get("/jobs",async(req,res)=>{
+    try{
+        const job=await Job.find().lean().exec()
+        res.status(200).send({job})
+    }catch(e){
+        res.status(500).send({message:e.message})
+    }
+})
+
+app.post("/jobs",async(req,res)=>{
+    try{
+        const job=await Job.create(req.body)
+        res.status(201).send(job)
+    }catch(e){
+        res.status(500).send({message:e.message})
+    }
+})
+
+app.get("/jobs/:id",async(req,res)=>{
+    try{
+        const job=await Job.findById(req.params.id).lean().exec()
+        res.status(200).send({job})
+    }catch(e){
+        res.status(500).send({message:e.message})
+    }
+})
+
+
+app.patch("/jobs/:id",async(req,res)=>{
+    try{
+        const job=await Job.findByIdAndUpdate(req.params.id,req.body,{new:true}).lean().exec()
+        res.status(200).send({job})
+    }catch(e){
+        res.status(500).send({message:e.message})
+    }
+})
+
+app.delete("/jobs/:id",async(req,res)=>{
+    try{
+        const job=await Job.findByIdAndDelete(req.params.id).lean().exec()
+        res.status(200).send({job})
+    }catch(e){
+        res.status(500).send({message:e.message})
+    }
+})
 
 app.listen(5000,async()=>{
     await connect();
